1fa2524b6eb44444a4bc6e3bc85b46ee8528b5b9
Oscar Martins <oscarrafaelcampos@gmail.com>
2018-01-24 17:33:37 +0000

    context layout

--- pages/index.vue
+++ pages/index.vue
@@ -51,8 +51,8 @@
                     </div>
                 </Menu>
             </Header>
-            <Content :style="{margin: '88px 20px 0', background: '#fff', minHeight: '500px'}">
-                Content
+            <Content :style="{margin: '88px 20px 0', background: '#fff'}">
+                Content 2
             </Content>
             <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
         </Layout>
