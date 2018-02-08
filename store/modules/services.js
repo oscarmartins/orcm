4fc2422f25753639a4c761dc77c8271428619305
Oscar Martins <oscarrafaelcampos@gmail.com>
2018-02-08 18:24:14 +0000

    services store

--- store/modules/services.js
+++ store/modules/services.js
@@ -5,11 +5,65 @@ const parameters = (rc, ra, ri) => { return {REQ_CONTEX: rc, REQ_ACTION: ra, REQ
 const services = {
   namespaced: true,
   state: {
-    user: {},
-    isSigned: {}
+    token: null,
+    user: null
   },
-  mutations: {},
+  mutations: {
+    // SET_USER
+    SET_USER (state, user) {
+        state.user = user
+    },
+    // SET_TOKEN
+    SET_TOKEN (state, token) {
+      state.token = token
+    }
+  },
   actions: {
+
+      // Update token
+    async updateToken ({ commit }, token) {
+        // Update token in store's state
+        commit('SET_TOKEN', token)
+  
+        // Set Authorization token for all axios requests
+        this.$axios.setToken(token, 'Bearer')
+  
+        
+        // Update localStorage
+        if (process.browser && localStorage) {
+          if (token) {
+            localStorage.setItem('token', token)
+          } else {
+            localStorage.removeItem('token')
+          }
+        }
+        
+  
+        
+        // Update cookies
+        if (process.browser) {
+          // ...Browser
+          if (token) {
+            Cookies.set('token', token)
+          } else {
+            Cookies.remove('token')
+          }
+        } else {
+          // ...Server
+          let params = {
+            domain: '/'
+          }
+          if (!token) {
+            let expires
+            let date = new Date()
+            expires = date.setDate(date.getDate() - 1)
+            params.expires = new Date(expires)
+          }
+          this.app.context.res.setHeader('Set-Cookie', Cookie.serialize('token', token, params))
+        }
+        
+      },
+
       async signup (context, payload) {
         let data = await this.$axios.$post(endpoint, parameters(1000, 1010, payload)).then(function(resp){
             return resp
@@ -46,6 +100,10 @@ const services = {
         return data
       }
   },
-  getters: {}
+  getters: {
+    loggedIn (state) {
+      return Boolean(state.user && state.token)
+    }
+  }
 }
 export default services
